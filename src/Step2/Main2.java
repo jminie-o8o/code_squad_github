45b12818582990a89606ecd1a3a2660073a3fd4a
hwj <jminie@naver.com>
2021-12-07 23:12:44 +0900

    add Move.java and moveRight method for Step3

--- src/Step2/Main2.java
+++ src/Step2/Main2.java
@@ -11,11 +11,16 @@ public class Main2 {
         MapConvert mapConvert = new MapConvert();
         Input input = new Input();
         Play play = new Play();
+        Show show = new Show();
         PlayAgain playAgain = new PlayAgain();
 
         int[][] secondStage = mapConvert.convert(mapList.stage2());
         boolean again = true;
         while (again){
+            System.out.println("Stage 2");
+            System.out.println();
+            show.showMap(mapList.stage2());
+            System.out.println();
             List<String> command = input.input();
             play.playGame(command, secondStage);
             again = playAgain.again();
